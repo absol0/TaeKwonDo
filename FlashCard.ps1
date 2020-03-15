fdff920db239dea902188beb741341d47f9a83d0
Alan Bill <abill@qti.qualcomm.com>
2020-03-15 07:14:38 +0000

    Update FlashCard.ps1
    Cleanup of code

--- FlashCard.ps1
+++ FlashCard.ps1
@@ -4,11 +4,8 @@ do {
 	$Test = Get-Random -InputObject $Words
 	"$($test.Kup) Kup, $($Test.Section)"
 	"$($Test.English)"
-	$Host.UI.RawUI.FlushInputBuffer()
 	$KeyPressed = $host.UI.RawUI.ReadKey()
 	"$($Test.Korean)"
-	$Host.UI.RawUI.FlushInputBuffer()
 	$KeyPressed = $host.UI.RawUI.ReadKey()
-	#"<$($KeyPressed)>"
 } until ($KeyPressed.Character -eq 'q')
 
